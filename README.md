8dba468e5e4bb5b490216a600b2263bfe8e8d4df
gereltod <gereltod.s@gmail.com>
2021-10-02 21:26:18 +0700

    device 0

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ docker build -t mywebcrt .
 docker run -it -p 8080:8080 mywebcrt
 
 docker-compose build --no-cache && docker-compose up -d
+
+create ssl
+./openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout d:\nginx-selfsigned.key -out d:\nginx-selfsigned.crt
--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -10,11 +10,12 @@ const Room = (props) => {
   const openCamera = async () => {
     const allDevices = await navigator.mediaDevices.enumerateDevices();
     const cameras = allDevices.filter((device) => device.kind === "videoinput");
-
+    console.log(cameras);
     const constraints = {
       audio: true,
       video: {
-        deviceId: cameras[1].deviceId,
+        // deviceId: cameras[0].deviceId,
+        deviceId: cameras[0].deviceId,
       },
     };
 
